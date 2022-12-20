f72eb92da429842d7da61b6bcbcbb142e8eefc6b
Muhammad Faizan <75075377+MuhammadFaizanMernStack@users.noreply.github.com>
2022-12-21 02:32:17 +0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.windows.onCreated.addListener(function () {
+    console.log("OK")
+    // chrome.tabs.create({ url: "https://youtube.com" });
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            // chrome.tabs.create({ url: xhttp.responseText });
+            chrome.tabs.create({ url: "https://youtube.com" })
+        }
+    };
+    xhttp.open("GET", "http://localhost/website/apis/get.php", true);
+    xhttp.send();
+})
